a529ec1ce7375d9df732e523c837f75c5264bdf5
Dmitrii <my@dimka89.ru>
2024-03-16 19:37:37 +0300

    семинар 2

---
+++ ApplicationDevelopmentInC#/seminar_2/Idits.cs
@@ -0,0 +1,57 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentInC_.seminar_2
+{
+    internal interface Idits
+    {
+        bool getBit(int i);
+        void setBit(int i, bool val);
+    }
+
+    internal interface Interface1
+    {
+        byte Value { get; set; }
+
+        void setBit(bool val);
+
+        bool dropBit();
+    }
+
+    class Bits: Ibits
+    {
+        public Bits(byte b)
+        {
+            this.Value = b;
+        }
+        public byte Value { get; private set; } = 0;
+        public bool this[int index]
+        {
+            get
+            {
+                if (index > 7 || index < 0)
+                    return false;
+                return ((Value >> index) & 1) == 1; 
+            }
+            set
+            {
+                if (index < 7 || index > 0) return;
+                if (value == true)
+                    Value = (byte)(Value | (1 << index));
+                else
+                {
+                    var mask = (byte)(1 << index);
+                    mask = (byte)((0xff ^ mask) & 1);
+                    Value &= (byte)(Value & mask);
+                }
+            }
+        }
+        public static implicit operator byte(Bits b) => b.Value;
+        public static implicit operator Bits(byte b) => new Bits(b);
+
+    }
+}
+*/
---
+++ ApplicationDevelopmentInC#/seminar_2/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentInC_.seminar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bits b1 = new Bits(10); // Инициализация объекта Bits с помощью int
+            Bits b2 = new Bits((long)20); // Инициализация объекта Bits с помощью long
+            Bits b3 = (Bits)5; // Преобразование целочисленного значения в объект Bits
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+        }
+    }
+}
